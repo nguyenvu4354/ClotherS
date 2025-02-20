a75b0653564f5d536e8e17c3691ce284f9c88138
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-02-20 13:46:20 +0700

    Create mangae Product

---
+++ Migrations/20250220055744_SeedOrders.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClotherS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "Accounts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
