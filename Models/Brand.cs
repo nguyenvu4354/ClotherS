d75b7bece154b737da35f4382462da7bf9c1d089
DESKTOP-MAC88L0\nguye <nguyenvu150203@gmail.com>
2025-03-04 21:44:14 +0700

    fix front end

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ClotherS.Models
 {
@@ -6,9 +7,15 @@ namespace ClotherS.Models
     {
         [Key]
         public int BrandId { get; set; }
+
+        [Required, StringLength(100)]
         public string BrandName { get; set; }
 
-        public ICollection<Product> Products { get; set; }
-    }
+        [StringLength(500)] 
+        public string Description { get; set; }
 
+        public bool Disable { get; set; } = false; 
+
+        public ICollection<Product> Products { get; set; } = new List<Product>(); 
+    }
 }
